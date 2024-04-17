d48cc2a43e89d6a098300ded0b61c3941aaa3ee5
Andris Dobičinaitis <root@dobicinaitis.dev>
2024-04-18 01:28:00 +0300

    Added a utility script that applies simple transformations to OCR'd text.

---
+++ utils/ocr-fixup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Apply some simple transformations to OCR'd text.
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRATCH_FILE=$SCRIPT_DIR/scratchpad
+
+if [ ! -f "$SCRATCH_FILE" ]; then
+    echo "Scratch file not found: $SCRATCH_FILE"
+    exit 1
+fi
+
+# remove hyphenated word breaks
+sed -i ':a;N;$!ba;s/-\n//g' "$SCRATCH_FILE"
+
+# join sentences that were split across lines
+sed -i ':a;N;$!ba;s/\([^\n]\)\(\n\)\(^[a-z]\)/\1 \3/g' "$SCRATCH_FILE"
